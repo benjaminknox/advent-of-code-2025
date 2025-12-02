40c412102b78248362bf7a3aa32b9c47a9bc6a91
Benjamin Knox <bknox.contact@gmail.com>
2025-12-02 07:41:58 -0500

    [Day 2]: Part one

---
+++ day-2/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, Write};
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let default_value = "my-input.txt";
+
+    print!("Enter input file: (default: {}): ", default_value);
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    input = input.trim().to_string();
+
+    if input.is_empty() {
+        input = default_value.to_string();
+    }
+
+    let file = File::open(input).expect("Failed to open file");
+
+    let reader = BufReader::new(file);
+
+    let code_list = reader.lines()
+        .next()
+        .expect("File is empty")
+        .expect("Failed to read line");
+
+    let mut result : i64 = 0;
+
+    for code_range_string in code_list.split(',') {
+        let code_range_nums: Vec<&str> = code_range_string.split('-').collect();    
+        let start: i64 = code_range_nums[0].parse().expect("Failed to parse start");
+        let end: i64 = code_range_nums[1].parse().expect("Failed to parse end");
+
+        for code in start..=end {
+            let code_str : String = code.to_string();
+
+            let code_length = code_str.len();
+            
+            if code_length % 2 == 0 {
+                let half_code_length : usize = code_length / 2;
+                let first_half : &str = &code_str[..half_code_length];
+                let second_half : &str = &code_str[half_code_length..];
+
+                if first_half == second_half {
+                    println!("{}: {}={}", code_str, first_half, second_half);
+
+                    result += code;
+                }
+            }
+        }
+    }
+
+    println!("Sum of all invalid ids: {}", result);
+
+}
