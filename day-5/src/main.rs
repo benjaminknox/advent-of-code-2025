0c40307bb1225e49951b0c2f0db1baa00492cdc5
Benjamin Knox <bknox.contact@gmail.com>
2025-12-05 07:50:15 -0500

    [Day 5]: Part two

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -23,15 +23,34 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut parsing_ranges : bool = true;
+    let mut total_fresh_ingredients_for_part_1 : i64 = 0;
+    let mut total_fresh_ingredients_for_part_2 : i64 = 0;
     let mut ingredient_id_ranges : Vec<(i64,i64)> = Vec::new();
-    let mut total_fresh_ingredients : i64 = 0;
+    let mut merged_ingredient_id_ranges : Vec<(i64,i64)> = Vec::new();
 
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
-        println!("line: {}", line);
 
         if line.is_empty() {
             parsing_ranges = false;
+            ingredient_id_ranges.sort_by_key(|range| range.0);
+
+            for &(range_start, range_end) in ingredient_id_ranges.iter() {
+                if merged_ingredient_id_ranges.len() == 0 {
+                    merged_ingredient_id_ranges.push((range_start, range_end))
+                } else {
+                    let last_range = merged_ingredient_id_ranges.last_mut().unwrap();
+                    
+                    if range_start <= last_range.1 {
+                        last_range.1 = last_range.1.max(range_end);
+                    } else {
+                        merged_ingredient_id_ranges.push((range_start, range_end))
+                    }
+                }
+            }
+
+            total_fresh_ingredients_for_part_2 = merged_ingredient_id_ranges.iter().map(|(range_start, range_end)| range_end - range_start + 1).sum();
+
         }
 
         if parsing_ranges {
@@ -46,13 +65,14 @@ fn main() {
 
             for &(range_start, range_end) in ingredient_id_ranges.iter() {
                 if range_start <= ingredient_id && ingredient_id <= range_end {
-                    println!("ingredeint_id: {}, range_start: {}, range_end: {}", ingredient_id, range_start, range_end);
-                    total_fresh_ingredients += 1;
+                    total_fresh_ingredients_for_part_1 += 1;
                     break;
                 }
             }
+
         }
     }
 
-    println!("Sum of all total fresh ingredients: {}", total_fresh_ingredients);
+    println!("Sum of all total fresh ingredients for part 1: {}", total_fresh_ingredients_for_part_1);
+    println!("Sum of all total fresh ingredients for part 2: {}", total_fresh_ingredients_for_part_2);
 }
